772d90fc62a4cd0a0196e9ec8ecb0afe639561a6
alexfrank10 <alexiano5161@gmail.com>
2024-11-20 00:00:30 +0100

    model.pkl added

--- src/Components/data_ingestion.py
+++ src/Components/data_ingestion.py
@@ -9,6 +9,10 @@ from dataclasses import dataclass
 from src.Components.data_transformation import DataTranformationConfig
 from src.Components.data_transformation import DataTransformation
 
+from src.Components.model_trainer import ModelTrainer
+from src.Components.model_trainer import ModelTrainerConfig
+
+
 
 @dataclass
 class DataIngestionConfig:
@@ -55,6 +59,9 @@ if __name__=="__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr, test_arr, _=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
             
